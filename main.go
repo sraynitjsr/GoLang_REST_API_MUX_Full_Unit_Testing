6e54d00a38fbfded23668ef9ee0e12d6685b8d25
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-04-07 16:50:42 +0530

    Unit Tesing in GoLang Using Ginkgo and Gomega

--- main.go
+++ main.go
@@ -2,35 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/sraynitjsr/model"
 )
 
 func main() {
 	fmt.Println("Unit Tesing in GoLang Using Ginkgo and Gomega")
-
-	repo := model.NewInMemoryStudentRepository()
-
-	service := model.NewStudentService(repo)
-
-	student := &model.Student{
-		Name: "Subhradeep Ray",
-		Roll: 2020,
-	}
-
-	err := service.Add(student)
-
-	if err != nil {
-		fmt.Printf("Error adding student: %s", err.Error())
-		return
-	}
-
-	addedStudent, err := service.Get(student.Roll)
-
-	if err != nil {
-		fmt.Printf("Error retrieving student: %s", err.Error())
-		return
-	}
-
-	fmt.Printf("Added student: name=%s, roll=%d\n", addedStudent.Name, addedStudent.Roll)
 }
