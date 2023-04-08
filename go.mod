e072a7753ffd2e451d2312047197a9416f2cdb49
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-04-08 11:46:59 +0530

    Created / default route

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sraynitjsr
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("GoLang Gorilla MUX REST API")
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(responseWriter, "Home Handler, Welcome...............")
+	})
+	log.Println("Starting Server at Port 8000")
+	log.Fatalln(http.ListenAndServe(":8000", router))
 }
